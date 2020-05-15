b78dacfd6c7f96a177829370b4a1eda00ef85369
Justin Ong <justin.ong.97@gmail.com>
2020-05-15 17:05:27 +0800

    Add course selector

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Layout, Select, Tag } from 'antd';
+import 'antd/dist/antd.less';
 import './App.css';
 
 import { ICourse } from './logic/interface.d';
 
 interface IAppState {
-  courses: ICourse[];
+  courses: {
+    [key: string]: ICourse;
+  };
 }
 
 export default class App extends React.Component<{}, IAppState> {
@@ -12,17 +16,47 @@ export default class App extends React.Component<{}, IAppState> {
     super(props);
 
     this.state = {
-      courses: []
+      courses: {}
     };
   }
 
   componentDidMount() {
-    fetch('courses.json').then((courses) => {
-      this.setState({ courses: courses as any });
-    });
+    fetch('courses.json')
+      .then((res) => res.json())
+      .then((courses) => {
+        this.setState({ courses: courses as any });
+      });
   }
 
+  onSelect = (value: any, option: any) => {
+    console.log('+', value);
+  };
+
+  onDeselect = (value: any, option: any) => {
+    console.log('-', value);
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <Layout>
+        <Layout.Content>
+          <Select
+            mode="multiple"
+            placeholder="Select Courses"
+            style={{ width: '100%' }}
+            onSelect={this.onSelect}
+            onDeselect={this.onDeselect}
+            optionLabelProp="value"
+          >
+            {Object.values(this.state.courses).map((course: ICourse) => (
+              <Select.Option key={course.code} value={course.code}>
+                {`${course.code} - ${course.name}`.replace(/\*?#?$/, '')}
+              </Select.Option>
+            ))}
+          </Select>
+          asdf
+        </Layout.Content>
+      </Layout>
+    );
   }
 }
